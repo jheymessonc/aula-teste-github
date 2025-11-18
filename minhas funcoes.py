6a0472e251f7dc56c9edaaaa6ac540925aa8b82a
JHEYMESSON APOLINARIO CAVALCANTI <jheymesson.cavalcanti@unicap.br>
2025-11-18 08:39:37 -0300

    adicao de filtro de espaco e link e botao no html

--- minhas funcoes.py
+++ minhas funcoes.py
@@ -3,3 +3,10 @@ def soma(numero1, numero2):
 
 def multiplicar(numero1, numero2):
     return numero1 * numero2
+
+def filtro_espaco(frase):
+    resposta = ''
+    for letra in frase:
+        if letra != ' ':
+            resposta += letra
+    return resposta
